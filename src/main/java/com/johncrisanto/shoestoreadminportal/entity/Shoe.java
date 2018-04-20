7a181652d48e510da462e56944eb6a34c983f5e0
John Crisanto <johnccrisanto@gmail.com>
2018-04-20 16:46:00 -0700

    Get landing page working on admin portal.

--- src/main/java/com/johncrisanto/shoestoreadminportal/entity/Shoe.java
+++ src/main/java/com/johncrisanto/shoestoreadminportal/entity/Shoe.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,7 +28,7 @@ public class Shoe {
 	private double shippingWeight;
 	private boolean active=true;
 	private boolean newArrival=true;
-	
+	@Transient
 	private MultipartFile shoeImage;
 	
 	public Shoe() {
